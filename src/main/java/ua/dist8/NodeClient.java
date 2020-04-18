af6d27b12aa4724f96a7b4cdabe6cdc6372aca45
JensD1 <jens.duym@student.uantwerpen.be>
2020-04-18 13:48:28 +0200

    Added a javadoc for all methods in NodeClient.

--- src/main/java/ua/dist8/NodeClient.java
+++ src/main/java/ua/dist8/NodeClient.java
@@ -34,7 +34,10 @@ public class NodeClient {
     }
 
     /**
-     * This function wil run when a node receives a multicast message from a new node that wants to join the network
+     * This function wil run when a node receives a multicast message from a new node that wants to join the network.
+     * Here we will see if the new node is a neighbour of the existing (current) node. When yes, we will adjust the
+     * next-/previous ID number of the current node and send a message back to the node to let it know we are
+     * neighbours.
      * @param receivedNodeName is the name of the node that wants to join
      * @param nodeIP is the IP-address of the node that wants to join
      */
@@ -73,6 +76,21 @@ public class NodeClient {
         }
     }
 
+    /**
+     * This method will send a unicast message to a given InetAddress as a respons to a multicast message.
+     * The JSON object that will be send contains
+     * 1) the type of node ( so the new node will know if this message comes from the NamingServer or another node ).
+     * 2) the boolean isEndNode ( so the new node will know if it becomes a new endnode of the network ).
+     * 3) the currentID ( this is the ID of the existing node in the network ).
+     * 4) the newNodeID ( this is the ID of the node that wants to enter the network ).
+     * @param toSend the InetAddress to were to send.
+     * @param currentID The ID of the node that is already in the network.
+     * @param newNodeID The ID of the node that wants to join the network.
+     * @param isEndNode This boolean will be true if the currentID corresponds to an "endnode",
+     *                  which is the node with the highest/lowest ID number in the network
+     * @throws IOException the exception to handle the outputStream exceptions
+     * @throws JSONException the exception to handle the JSON exceptions
+     */
     public void sendUnicastMessage(InetAddress toSend, Integer currentID, Integer newNodeID, Boolean isEndNode) throws IOException, JSONException {
         Socket socket = new Socket(toSend, 5000);
         OutputStream outputStream = socket.getOutputStream();
@@ -86,7 +104,15 @@ public class NodeClient {
         outputStream.close();
         socket.close();
     }
-  
+
+    /**
+     *  Once a multicast message is sent, the node that wants to access the network needs to listen to all responses.
+     *  In the case that this is the first node, there will be only one message from the NamingServer, otherwise
+     *  there will be 3 messages. 1 from the NamingServer and 2 from the other clients.
+     *  In this method the node that wants to access the network will listen en see where in the network it belongs.
+     * @throws Exception
+     * @throws JSONException
+     */
     public void receiveUnicastMessage() throws Exception, JSONException{
         Integer receivedNumberOfMessages = 0;
         Boolean leaveWhile = Boolean.FALSE;
@@ -134,6 +160,15 @@ public class NodeClient {
         }while(!leaveWhile && receivedNumberOfMessages<3);
     }
 
+    /**
+     * This method will send a multicast message over UDP to everyone listening to the multicastAddress 224.0.0.200.
+     * The purpose of this message is to make it possible for a node to access the current network and to know what
+     * nodes are already on the network.
+     * In this message, two variables will be transmitted by using a JSON object. The first variable will be the name
+     * of the node that wants to join the network, the second variable will be the InetAddress of this node.
+     * @throws IOException
+     * @throws JSONException
+     */
     public void multicast() throws IOException, JSONException {
         InetAddress MCgroup = InetAddress.getByName("224.0.0.200");
         JSONObject obj = new JSONObject();
@@ -148,6 +183,13 @@ public class NodeClient {
 
     }
 
+    /**
+     * In this method we will let the node scan for incoming UDP messages on the multicast address 224.0.0.200.
+     * When there is such an incoming message, there is a node that wants to join the network an we will let
+     * the receivedMulticast method handle this.
+     * @throws IOException
+     * @throws JSONException
+     */
     public void receiveMC () throws IOException, JSONException{
         MulticastSocket ms = new MulticastSocket(6012);
         InetAddress MCgroup = InetAddress.getByName("224.0.0.200");
