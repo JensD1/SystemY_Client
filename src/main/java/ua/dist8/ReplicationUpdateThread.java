7257ea48ac2c19c80e8a9a5916be910e79f53ad3
JensD1 <jens.duym@student.uantwerpen.be>
2020-05-18 21:12:33 +0200

    Debug

--- src/main/java/ua/dist8/ReplicationUpdateThread.java
+++ src/main/java/ua/dist8/ReplicationUpdateThread.java
@@ -1,5 +1,7 @@
 package ua.dist8;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
 
 import java.io.File;
@@ -10,7 +12,10 @@ import java.net.Socket;
 import java.util.Map;
 import java.util.Timer;
 
+
 public class ReplicationUpdateThread extends Thread{
+
+    private static final Logger logger = LogManager.getLogger();
 
     @Override
     /***
@@ -18,12 +23,12 @@ public class ReplicationUpdateThread extends Thread{
      * If change has occurred, then the replication map will be updated. Other nodes will be also notified if a file is locally deleted.
      */
     public void run() {
-
+        logger.info("ReplicationUpdateThread started.");
         long  startTime = System.currentTimeMillis();
         long  interval = 1000;
 
         if (System.currentTimeMillis()-startTime>=interval){
-
+            logger.info("Checking if files are removed or added.");
             Map<String, InetAddress> replicatedFilesMap = NodeClient.getReplicatedFilesMap();
             boolean remove;
             File folder = new File("/home/pi/localFiles/");
@@ -31,9 +36,9 @@ public class ReplicationUpdateThread extends Thread{
 
             for (File file : listOfFiles) {
                 if (file.isFile()) {
-
                     //Check if there are new files
                     if (!replicatedFilesMap.containsKey(file.getName())){
+                        logger.info("There is a file added! Sending it to the rightful owner.");
                         NodeClient.getInstance().sendFileAndCreatedLogFile(file);
                     }
 
@@ -44,7 +49,6 @@ public class ReplicationUpdateThread extends Thread{
                 remove = true;
                 for (File file : listOfFiles) {
                     if (file.isFile()) {
-
                         //Check if there are removed files
                         if (hashKey.equals(file.getName())){
                             remove = false;
@@ -54,24 +58,16 @@ public class ReplicationUpdateThread extends Thread{
                 }
 
                 if (remove){
-
+                    logger.info("There is a file removed! Deleting it on all other nodes.");
                     JSONObject json = new JSONObject();
-
-                    json.put("typeOfMsg","removeReplicatedFile");
+                    json.put("typeOfMsg","replicationShutdown");
+                    json.put("typeOfSource", "local");
                     json.put("typeOfDest","owner");
                     json.put("fileName",hashKey);
-
                     NodeClient.getInstance().sendUnicastMessage(replicatedFilesMap.get(hashKey),json);
-
                     replicatedFilesMap.remove(hashKey);
-
-
                 }
-
             }
-
         }
-
     }
-
 }
