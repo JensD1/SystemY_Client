d8dea5bf967dda7867223bbcf4f761c7af080c86
JensD1 <jens.duym@student.uantwerpen.be>
2020-05-18 21:34:34 +0200

    Debug

--- src/main/java/ua/dist8/ReplicationUpdateThread.java
+++ src/main/java/ua/dist8/ReplicationUpdateThread.java
@@ -23,49 +23,51 @@ public class ReplicationUpdateThread extends Thread{
      * If change has occurred, then the replication map will be updated. Other nodes will be also notified if a file is locally deleted.
      */
     public void run() {
+
         logger.info("ReplicationUpdateThread started.");
         long  startTime = System.currentTimeMillis();
         long  interval = 1000;
+        while(true) {
+            if (System.currentTimeMillis() - startTime >= interval) {
+                logger.info("Checking if files are removed or added.");
+                Map<String, InetAddress> replicatedFilesMap = NodeClient.getReplicatedFilesMap();
+                boolean remove;
+                File folder = new File("/home/pi/localFiles/");
+                File[] listOfFiles = folder.listFiles();
 
-        if (System.currentTimeMillis()-startTime>=interval){
-            logger.info("Checking if files are removed or added.");
-            Map<String, InetAddress> replicatedFilesMap = NodeClient.getReplicatedFilesMap();
-            boolean remove;
-            File folder = new File("/home/pi/localFiles/");
-            File[] listOfFiles = folder.listFiles();
-
-            for (File file : listOfFiles) {
-                if (file.isFile()) {
-                    //Check if there are new files
-                    if (!replicatedFilesMap.containsKey(file.getName())){
-                        logger.info("There is a file added! Sending it to the rightful owner.");
-                        NodeClient.getInstance().sendFileAndCreatedLogFile(file);
-                    }
-
-                }
-            }
-
-            for (String hashKey : replicatedFilesMap.keySet()) {
-                remove = true;
                 for (File file : listOfFiles) {
                     if (file.isFile()) {
-                        //Check if there are removed files
-                        if (hashKey.equals(file.getName())){
-                            remove = false;
+                        //Check if there are new files
+                        if (!replicatedFilesMap.containsKey(file.getName())) {
+                            logger.info("There is a file added! Sending it to the rightful owner.");
+                            NodeClient.getInstance().sendFileAndCreatedLogFile(file);
                         }
 
                     }
                 }
 
-                if (remove){
-                    logger.info("There is a file removed! Deleting it on all other nodes.");
-                    JSONObject json = new JSONObject();
-                    json.put("typeOfMsg","replicationShutdown");
-                    json.put("typeOfSource", "local");
-                    json.put("typeOfDest","owner");
-                    json.put("fileName",hashKey);
-                    NodeClient.getInstance().sendUnicastMessage(replicatedFilesMap.get(hashKey),json);
-                    replicatedFilesMap.remove(hashKey);
+                for (String hashKey : replicatedFilesMap.keySet()) {
+                    remove = true;
+                    for (File file : listOfFiles) {
+                        if (file.isFile()) {
+                            //Check if there are removed files
+                            if (hashKey.equals(file.getName())) {
+                                remove = false;
+                            }
+
+                        }
+                    }
+
+                    if (remove) {
+                        logger.info("There is a file removed! Deleting it on all other nodes.");
+                        JSONObject json = new JSONObject();
+                        json.put("typeOfMsg", "replicationShutdown");
+                        json.put("typeOfSource", "local");
+                        json.put("typeOfDest", "owner");
+                        json.put("fileName", hashKey);
+                        NodeClient.getInstance().sendUnicastMessage(replicatedFilesMap.get(hashKey), json);
+                        replicatedFilesMap.remove(hashKey);
+                    }
                 }
             }
         }
